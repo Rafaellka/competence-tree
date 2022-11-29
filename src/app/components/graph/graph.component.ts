6400ba54c26b9f2efd73d3a3c05e6e750cff70bd
PavelShershnoyv <pavel.shershnoyv.02@gmail.com>
2022-11-29 12:01:40 +0500

    init graph

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import {EChartsOption} from "echarts";
 
 @Component({
@@ -9,6 +9,7 @@ import {EChartsOption} from "echarts";
 export class GraphComponent implements OnInit {
   options: EChartsOption = {
     tooltip: {},
+
     animationDurationUpdate: 1500,
     animationEasingUpdate: 'quinticInOut',
     series: [ 
@@ -16,7 +17,7 @@ export class GraphComponent implements OnInit {
         type: 'graph',
         layout: 'none',
         symbolSize: 50,
-        roam: true,
+        roam: false,
         label: {
           show: true
         },
@@ -27,74 +28,237 @@ export class GraphComponent implements OnInit {
         },
         data: [
           {
-            name: 'Node 1',
+            id: '1',
+            name: 'Artsofte',
+            x: 500,
+            y: 300, 
+            label :{
+              color: 'white',
+            },
+            select: {
+              itemStyle: {
+                color: 'green'
+              }
+            },
+            symbolSize: 100,
+            itemStyle: {
+              color: '#2E62D9',
+              
+            }
+          },
+          {
+            id: '2',
+            name: 'Грейд 1',
             x: 300,
-            y: 300
+            y: 300,
+            itemStyle: {
+              color: '#A1DE93'
+            }
           },
           {
-            name: 'Node 2',
-            x: 800,
-            y: 300
+            id: '3',
+            name: 'Грейд 2',
+            x: 200,
+            y: 280,
+            itemStyle: {
+              color: '#F7F48B'
+            }
           },
           {
-            name: 'Node 3',
-            x: 550,
-            y: 100
+            id: '4',
+            name: 'Должность 1',
+            x: 300, 
+            y: 200,
+            itemStyle: {
+              color: '#A1DE93'
+            }
           },
           {
-            name: 'Node 4',
-            x: 550,
-            y: 500
+            id: '5',
+            name: 'Должность 2',
+            x: 180, 
+            y: 350,
+            label :{
+              // color: 'white'
+            },
+            itemStyle: {
+              color: '#70A1D7'
+            }
           },
           {
-            name: '213'
-          }
+            id: '6',
+            name: 'Скилл 1',
+            x: 350, 
+            y: 400,
+            itemStyle: {
+              color: '#F47C7C'
+              
+            }
+          },
+          {
+            id: '7',
+            name: 'Скилл 2',
+            x: 150, 
+            y: 200,
+            itemStyle: {
+              color: '#F47C7C'
+            }
+          },
+          {
+            id: '8',
+            name: 'Грейд 1',
+            x: 590, 
+            y: 340,
+            itemStyle: {
+              color: '#F7F48B'
+            }
+          },
+          {
+            id: '9',
+            name: 'Грейд 2',
+            x: 700, 
+            y: 400,
+            itemStyle: {
+              color: '#F7F48B'
+            }
+          },
+          {
+            id: '10',
+            name: 'Скилл 1',
+            x: 580, 
+            y: 400,
+            itemStyle: {
+              color: '#F47C7C'
+              
+            }
+          },
+          {
+            id: '11',
+            name: 'Скилл 2',
+            x: 720, 
+            y: 360,
+            itemStyle: {
+              color: '#F47C7C'
+            }
+          },
+          {
+            id: '12',
+            name: 'Должность 1',
+            x: 560, 
+            y: 280,
+            itemStyle: {
+              color: '#70A1D7'
+            }
+          },
+          {
+            id: '13',
+            name: 'Скилл 3',
+            x: 760, 
+            y: 460,
+            itemStyle: {
+              color: '#F47C7C'
+            }
+          },
+          {
+            id: '14',
+            name: 'Скилл 2',
+            x: 800, 
+            y: 400,
+            itemStyle: {
+              color: '#F47C7C'
+            }
+          },
+          {
+            id: '15',
+            name: 'Должность 2',
+            x: 770, 
+            y: 310,
+            itemStyle: {
+              color: '#70A1D7'
+            }
+          },
         ],
-        // links: [],
         links: [
           {
-            source: 0,
-            target: 1,
-            symbolSize: [5, 20],
-            label: {
-              show: true
-            },
+            source: '1',
+            target: '2',
             lineStyle: {
-              width: 5,
               curveness: 0.2
+            },
+            label: {
+              show: true,
+              formatter: () => {
+                return 'Frontend'
+              }
             }
           },
           {
-            source: 'Node 2',
-            target: 'Node 1',
-            label: {
-              show: true
-            },
-            lineStyle: {
-              curveness: 0.2
-            }
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 3'
+            source: '3',
+            target: '7'
             },
             {
-            source: 'Node 2',
-            target: 'Node 3'
+            source: '2',
+            target: '4'
           },
           {
-            source: 'Node 2',
-            target: 'Node 4'
+            source: '2',
+            target: '3'
           },
           {
-            source: 'Node 1',
-            target: 'Node 4'
+            source: '2',
+            target: '6'
+          },
+          {
+            source: '3',
+            target: '5'
+          },
+          {
+            source: '1',
+            target: '8',
+            lineStyle: {
+              curveness: -0.2
+            },
+            label: {
+              show: true,
+              formatter: () => {
+                return 'Backend'
+              }
+            }
+          },
+          {
+            source: '8',
+            target: '9'
+          },
+          {
+            source: '8',
+            target: '10'
+          },
+          {
+            source: '9',
+            target: '11'
+          },
+          {
+            source: '8',
+            target: '12'
+          },
+          {
+            source: '9',
+            target: '13'
+          },
+          {
+            source: '9',
+            target: '14'
+          },
+          {
+            source: '9',
+            target: '15',
+            lineStyle: {
+              curveness: -0.4
+            }
           }
         ],
-        lineStyle: {
-          opacity: 0.9,
-          width: 2,
-          curveness: 0
+        tooltip: {
+          trigger: 'none',
         }
       }
     ]
@@ -102,6 +266,11 @@ export class GraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(e: MouseEvent) {
+    console.log(e.offsetX)
+    console.log(e.offsetY)
   }
 
 }
